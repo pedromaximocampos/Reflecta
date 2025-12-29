0c6798e6266cad55c6266767f522413b9bacc60d
Pedro Maximo Campos do Carmo <pedromaximocc@gmail.com>
2025-12-29 13:44:15 -0300

    fix: arrumando bug esquecido

--- src/application/ports/messaging/ipassword_reset_publisher.py
+++ src/application/ports/messaging/ipassword_reset_publisher.py
@@ -8,5 +8,3 @@ class IPasswordResetPublisher(Protocol):
 
     async def publish_password_reset_requested(self, password_reset_event: PasswordResetRequested) -> None:
         ...
-
-    async def publish
